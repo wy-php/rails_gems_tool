9c074cb2ee69aa4b55200849098c3c1a476e9e68
王允 <906971957@qq.com>
2018-08-29 09:38:01 +0800

    修改了日志登记

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,7 +73,7 @@ set :unicorn_roles, [:db, :app, :web]
 
 #执行deploy中进行的操作
 # 在第一次部署的时候运行该命令,用来创建数据库。
-before "deploy:updated", "deploy:create_database"
+before "deploy:updated", "deploy:curd_database"
 #使用unicorn去运行该命令，如果是首次运行或者服务器端的unicorn进程挂掉的情况的话使用unicorn:start，其他的情况使用unicorn:restart
 after 'deploy:publishing', 'unicorn:restart'
 
@@ -83,7 +83,7 @@ namespace :deploy do
   # 自定义了一个部署任务, 即自动运行 rake RAILS_ENV=rails_env db:create
   # 其中 release_path 指的是当前 release 目录
   # `fetch(:rails_env)` 读取配置中的 rails_env 变量, 并在 rake 命令中带上 env 变量
-  task :create_database do
+  task :curd_database do
     on roles(:db) do
       within release_path do
         with rails_env: fetch(:rails_env) do
