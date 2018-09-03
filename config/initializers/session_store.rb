3d22f7847afa181edb160fc11c776167bcb1488d
王允 <906971957@qq.com>
2018-09-03 16:31:58 +0800

    F 修改session的配置

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 if Rails.env.development?
-  @redis_server = ["redis://:123456@localhost:6379/0/session"]
+  @redis_server = ["redis://localhost:6379/0/session"]
 elsif Rails.env.production?
   # redis配置连接的写法就是密码在前面，@后面跟着的是host+端口号+数据库+命名空间
-  @redis_server = ["redis://:rails_gems_tool@localhost:6379/0/session"]
+  @redis_server = ["redis://:rails_gems_tool@localhost:6378/0/session"]
 end
 Rails.application.config.session_store :redis_store,
                                                 servers: @redis_server,
